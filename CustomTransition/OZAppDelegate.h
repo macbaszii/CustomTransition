f36905dcbef33870602a99dbfdba4b99fb75252c
Bas <bas@oozou.com>
2014-04-29 16:48:48 +0700

    Initial Commit

---
+++ CustomTransition/OZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OZAppDelegate.h
+//  CustomTransition
+//
+//  Created by Kiattisak Anoochitarom on 4/29/2557 BE.
+//  Copyright (c) 2557 Kiattisak Anoochitarom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CustomTransition/OZViewController.h
@@ -0,0 +1,13 @@
+//
+//  OZViewController.h
+//  CustomTransition
+//
+//  Created by Kiattisak Anoochitarom on 4/29/2557 BE.
+//  Copyright (c) 2557 Kiattisak Anoochitarom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OZViewController : UIViewController
+
+@end
